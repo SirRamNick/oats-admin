6cea7c85b567cd700cc1c90deaa942dd4c109e51
GadianoC <cgadiano22a-0029@olopsc.edu.ph>
2024-12-16 17:47:13 +0800

    change profile page

--- lib/components/admin_color_theme.dart
+++ lib/components/admin_color_theme.dart
@@ -5,18 +5,18 @@ ThemeData adminTheme = ThemeData(
   primaryColor: const Color(0xFF0B085F),
   scaffoldBackgroundColor: const Color(0xFFFFD22F),
   primaryTextTheme: const TextTheme(
-    bodySmall: TextStyle(color: Colors.white),
-    bodyMedium: TextStyle(color: Colors.white),
-    bodyLarge: TextStyle(color: Colors.white),
+    bodySmall: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+    bodyMedium: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
+    bodyLarge: TextStyle(color: Colors.white, fontFamily: 'OpenSans'),
   ),
   colorScheme: ColorScheme.fromSwatch(
     backgroundColor: const Color(0xFFFFD22F),
     // backgroundColor: const Color(0xFFE2E2E2),
   ),
   textTheme: const TextTheme(
-    bodySmall: TextStyle(color: Colors.black),
-    bodyMedium: TextStyle(color: Colors.black),
-    bodyLarge: TextStyle(color: Colors.black),
+    bodySmall: TextStyle(color: Colors.black, fontFamily: 'OpenSans'),
+    bodyMedium: TextStyle(color: Colors.black, fontFamily: 'OpenSans'),
+    bodyLarge: TextStyle(color: Colors.black, fontFamily: 'OpenSans'),
   ),
   appBarTheme: const AppBarTheme(
     // backgroundColor: Color(0xFFE2E2E2),
@@ -32,7 +32,7 @@ ThemeData adminTheme = ThemeData(
   ),
   dialogTheme: const DialogTheme(
     backgroundColor: Color(0xFFE2E2E2),
-    titleTextStyle: TextStyle(color: Colors.black),
+    titleTextStyle: TextStyle(color: Colors.black, fontFamily: 'OpenSans'),
   ),
   textButtonTheme: TextButtonThemeData(
     style: TextButton.styleFrom(
